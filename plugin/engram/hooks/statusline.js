4d5d722f899c25f0fa55ea2dfd15b14573ef7785
Kirill Turanskiy <thebtf@users.noreply.github.com>
2026-04-23 04:09:59 +0300

    fix: align v5 hook contracts

--- plugin/engram/hooks/statusline.js
+++ plugin/engram/hooks/statusline.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
-process.stdout.write('[engram] ○ v5 cleanup in progress\n');
+const lib = require('./lib');
+
+function renderStatusline() {
+  return '[engram] ○ v5 cleanup in progress';
+}
+
+if (require.main === module) {
+  (async () => {
+    await lib.RunStatuslineHook(async () => renderStatusline(), renderStatusline);
+  })();
+}
+
+module.exports = {
+  renderStatusline,
+};
